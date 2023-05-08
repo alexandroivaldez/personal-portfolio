438ec4fce0fb96c04068e1d05bf18faf3523071b
Alexandro Valdez <alexandroivaldez@gmail.com>
2023-05-07 20:45:32 -0500

    Added menu options for left menu bar

--- src/components/LeftMenu/LeftMenu.jsx
+++ src/components/LeftMenu/LeftMenu.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 
+import { Icon } from '@iconify/react';
 import "./LeftMenu.css";
 
 function LeftMenu() {
   return (
     <div className='left-menu-container'>
       <h1>AV</h1>
-      <div>
-        <h1>Explore</h1>
+      <div className='menu-container'>
+        <Icon icon="mdi:hashtag" color="#ccc" width="25" height="25" />
+        <h3>Explore</h3>
       </div>
-      <div>
-        <h1>Settings</h1>
+      <div className='menu-container'>
+        <Icon icon="material-symbols:settings" color="#ccc" width="25" height="25" />
+        <h3>Settings</h3>
       </div>
     </div>
   )
