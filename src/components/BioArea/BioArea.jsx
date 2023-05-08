3e66fcb5360e7e7230e64f432235f66a4cdd9bc2
Alexandro Valdez <alexandroivaldez@gmail.com>
2023-05-08 12:38:06 -0500

    Adjusted styling

--- src/components/BioArea/BioArea.jsx
+++ src/components/BioArea/BioArea.jsx
@@ -12,7 +12,8 @@ function BioArea({name, username}) {
             <Icon icon="emojione-monotone:white-heavy-check-mark" color="#2393fa" />
         </div>
         <h5 style={{color: "#ccc"}}>@alexandroivaldez</h5>
-        <h4>Front-end Developer. Code Instructor at Mimo Dev. <span style={{color:"#2393FA"}}>#opentowork</span></h4>
+        <h4>Front-end Developer. Code Instructor at Mimo Dev 🪴 💻 ☕</h4>
+        <a href="https://www.linkedin.com/in/alexandro-valdez/"><span style={{display: "flex", alignItems:"center", color:"#2393FA"}}>#opentowork<Icon icon="mdi:linkedin" color="#0b5de0" /></span></a>
         <div className='bio-extras-container'>
             <div className="bio-extra">
                 <Icon icon="bi:geo-alt" color="#a6a6a6" width="20" height="20" />
@@ -20,7 +21,7 @@ function BioArea({name, username}) {
             </div>
             <div className="bio-extra">
                 <Icon icon="ic:outline-insert-link" color="#a6a6a6" width="20" height="20" />
-                <p style={{color:"#2393FA"}}>github.com</p>
+                <p><a style={{color:"#2393FA"}} href="https://github.com/alexandroivaldez">github.com</a></p>
             </div>
             <div className="bio-extra">
                 <Icon icon="ion:balloon-outline" color="#a6a6a6" width="20" height="20" />
