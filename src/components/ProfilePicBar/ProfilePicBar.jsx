b54c39406480da710ad79c9e12e38c6340f3b017
Alexandro Valdez <alexandroivaldez@gmail.com>
2023-05-09 23:36:32 -0500

    Update ProfilePicBar.jsx

--- src/components/ProfilePicBar/ProfilePicBar.jsx
+++ src/components/ProfilePicBar/ProfilePicBar.jsx
@@ -5,7 +5,7 @@ import "./ProfilePicBar.css";
 function ProfilePicBar() {
   return (
     <div className='profile-pic-container'>
-        <img src="profile.JPG" id="profile-pic" />
+        <img src="profile.jpg" id="profile-pic" />
         <button class="follow-btn">Follow</button>
     </div>
   )
