c2d84c2b6208d0bbb2ea10d34004589c7a2c6416
Alexandro Valdez <alexandroivaldez@gmail.com>
2023-05-09 22:37:01 -0500

    Added media quieres

--- src/components/BioArea/BioArea.jsx
+++ src/components/BioArea/BioArea.jsx
@@ -12,7 +12,7 @@ function BioArea({name, username}) {
             <Icon icon="mdi:check-decagram" color="#1da1f2" />
         </div>
         <h5 style={{color: "#ccc"}}>@alexandroivaldez</h5>
-        <h4>Front-end Developer. Code Instructor <a href="https://twitter.com/getmimo"><span style={{color:"#2393FA"}}>@getmimo</span></a> 🪴 💻 ☕</h4>
+        <h4>Front-end Developer for hire. Code Instructor <a href="https://twitter.com/getmimo"><span style={{color:"#2393FA"}}>@getmimo</span></a> 🪴💻</h4>
         <a href="https://www.linkedin.com/in/alexandro-valdez/"><span style={{display: "flex", alignItems:"center", color:"#2393FA"}}>#opentowork<Icon icon="mdi:linkedin" color="#0b5de0" /></span></a>
         <div className='bio-extras-container'>
             <div className="bio-extra">
@@ -20,7 +20,7 @@ function BioArea({name, username}) {
                 <p><a href="https://www.google.com/search?q=Houston%2C+Texas&oq=Houston%2C+Texas&aqs=chrome..69i57j46i131i433i512j46i433i512j0i131i433i512j0i433i512j69i61j69i60l2.2774j0j7&sourceid=chrome&ie=UTF-8">Houston, Texas</a></p>
             </div>
             <div className="bio-extra">
-                <Icon icon="ic:outline-insert-link" color="#a6a6a6" width="20" height="20" />
+                <Icon icon="ic:outline-insert-link" color="#a6a6a6" width="20" height="20" id="linkSVG" />
                 <p><a style={{color:"#2393FA"}} href="https://github.com/alexandroivaldez">github.com</a></p>
             </div>
             <div className="bio-extra">
--- src/components/LeftMenu/LeftMenu.jsx
+++ src/components/LeftMenu/LeftMenu.jsx
@@ -6,14 +6,13 @@ import "./LeftMenu.css";
 function LeftMenu() {
   return (
     <div className='left-menu-container' >
-      <h1>AV</h1>
       <div className='menu-container'>
         <Icon icon="mdi:hashtag" color="#ccc" width="25" height="25" />
-        <h3>Explore</h3>
+        <h3 id="explore-text">Explore</h3>
       </div>
       <div className='menu-container'>
         <Icon icon="material-symbols:settings" color="#ccc" width="25" height="25" />
-        <h3>Settings</h3>
+        <h3 id="settings-text">Settings</h3>
       </div>
     </div>
   )
